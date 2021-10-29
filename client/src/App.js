ccbecf16a812d2b04825bbe74fe01ecee3674672
Matt Hendrian <83432764+matthendrian@users.noreply.github.com>
2021-10-29 17:09:13 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ const client = new ApolloClient({
 
 function App() {
   return (
-     <ApolloProvider client={client}>
+    <ApolloProvider client={client}>
     <AuthState>
       <Router>
         <Fragment className="App">
