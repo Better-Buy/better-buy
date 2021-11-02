8baf8e494b48a8bb523efe0a424560a5ee7b8f7a
Jorge <82856506+jcant770@users.noreply.github.com>
2021-11-02 18:44:58 -0400

    add cart provider and stripe promise

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,9 @@ import { setContext } from '@apollo/client/link/context';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import PrivateRoute from './components/routing/PrivateRoute';
-
+import { loadStripe } from '@stripe/stripe-js';
+import { CartProvider } from "use-shopping-cart";
+import { Toaster } from 'react-hot-toast';
 
 import AuthState from './context/auth/authState';
 import AlertState from './context/alert/AlertState';
@@ -36,8 +38,11 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
+  from,
 } from '@apollo/client';
 
+const queryClient = new QueryClient();
+const stripePromise = loadStripe('pk_test_51Jq4qZGzUjXx6ZT6RJDT6629lmeCT3QuFPg4JrDbQML31wlbTIKlZhRRvaYQBuiHFDI5jGbA36gPCadnZ1SgcCGk00rncH3LQT');
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -60,7 +65,7 @@ const client = new ApolloClient({
 
 
 
-=======
+
 // Amir Token
 if(localStorage.token) {
   setAuthToken(localStorage.token);
@@ -69,8 +74,12 @@ if(localStorage.token) {
 
 function App() {
   return (
-    
     <ApolloProvider client={client}>
+      <CartProvider
+        mode="checkout-session"
+        stripe={stripePromise}
+        currecty="USD"
+      > 
 
     <AuthState>
 
@@ -78,6 +87,7 @@ function App() {
         <Router>
           <Fragment className="App">
             <Navbar />
+            <Toaster position="bottom-center"/>
             <SearchBar />
             <div className='conainter'>
               <Alerts />
@@ -96,6 +106,7 @@ function App() {
       </AlertState>
 
     </AuthState>
+      </CartProvider>
    </ApolloProvider>
   );
 }
