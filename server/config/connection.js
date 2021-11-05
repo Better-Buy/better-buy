ee359266d83998603b2166629fa3fd824ffc2f89
Amir Ahmadi <eamahma@gmail.com>
2021-11-04 21:51:37 -0400

    heroku put back working one

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,40 @@
+//creating a mongoose connection. 
+const express = require("express");
 const mongoose = require('mongoose');
+const app = express();
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
-  mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://eamahma:ANvfEEWfDOirWzc9@better-buy.sw2vw.mongodb.net/myFirstDatabase?retryWrites=true', {
+//updated mongodb to fix proxy error. 
+/*
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
+*/
+
+//This is our Mongo Atlas Connection: 'mongodb+srv://matt:Arsenal12@merncluster.gkugt.mongodb.net/test'
+
+//adding a port that MongoDB can utilize. 
+
+const PORT = process.env.PORT || 3000;
+
+const URI = process.env.MONGODB_URL;
+
+// mongoose.connect(URI || 'mongodb://localhost/mernshopping', {
+  mongoose.connect(URI || 'mongodb+srv://eamahma:WauksRO7rijDjO7X@better-buy.sw2vw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+useNewUrlParser: true, 
+
+useUnifiedTopology: true 
+
+}, err => {
+if(err) throw err;
+console.log('Connected to MongoDB!!!')
+});
+
+// app.listen(PORT, () => {
+//     console.log(`App running on port ${PORT}!`);
+// });
 
 module.exports = mongoose.connection;
 
