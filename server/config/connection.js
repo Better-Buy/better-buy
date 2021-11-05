0f3c6a0575d051c63f21f7b926c7c7881c98c89d
Amir Ahmadi <eamahma@gmail.com>
2021-11-04 21:36:24 -0400

    heroku deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://eamahma:ANvfEEWfDOirWzc9@better-buy.sw2vw.mongodb.net/myFirstDatabase?retryWrites=true', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
