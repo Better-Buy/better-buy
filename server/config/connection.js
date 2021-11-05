fc67ae6848a16433b37efd951853f3c40dde5439
Amir Ahmadi <eamahma@gmail.com>
2021-11-04 21:14:50 -0400

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,38 +1,11 @@
-//creating a mongoose connection. 
-const express = require("express");
 const mongoose = require('mongoose');
-const app = express();
 
-//updated mongodb to fix proxy error. 
-/*
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
-*/
-
-//This is our Mongo Atlas Connection: 'mongodb+srv://matt:Arsenal12@merncluster.gkugt.mongodb.net/test'
-
-//adding a port that MongoDB can utilize. 
-
-const PORT = process.env.PORT || 3000;
-
-const URI = process.env.MONGODB_URL;
-
-mongoose.connect(URI || 'mongodb://localhost/mernshopping', {
-useNewUrlParser: true, 
-
-useUnifiedTopology: true 
-
-}, err => {
-if(err) throw err;
-console.log('Connected to MongoDB!!!')
-});
-
-// app.listen(PORT, () => {
-//     console.log(`App running on port ${PORT}!`);
-// });
 
 module.exports = mongoose.connection;
+
