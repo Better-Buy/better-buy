1d3b13458bfda1a91ada554d152983d4bf10d442
Kevin Shank <Kevin_Shank@yahoo.com>
2021-11-17 00:13:12 -0500

    Add mutation for ADD_PRODUCT

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -48,6 +48,7 @@ const typeDefs = gql`
 
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addProduct(name: String!, description: String, image: String, price: Float!, sku: Int!): Product
     addOrder(products: [ID]!): Order
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     updateProduct(_id: ID!, quantity: Int!): Product
