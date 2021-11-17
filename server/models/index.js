12a6acc2f58091fea81050f88112002182ef24d6
Kevin Shank <Kevin_Shank@yahoo.com>
2021-11-16 23:01:13 -0500

    Update models

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,4 @@ const User = require('./User');
 const Product = require('./Product');
 const Order = require('./Order');
 
-module.exports = { User, Product, Category, Order };
+module.exports = { User, Product, Order };
--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
 });
 
 db.once('open', () => {
