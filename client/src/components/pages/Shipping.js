1de0897e8d231006e9b53296007237a13a8c9ffd
Kevin Shank <Kevin_Shank@yahoo.com>
2021-12-15 17:18:04 -0500

    Create breadcrumb for checkout steps

--- client/src/components/pages/Shipping.js
+++ client/src/components/pages/Shipping.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { saveShippingAddress } from '../../actions/cartActions'
+import Breadcrumb from '../layout/Breadcrumb'
 
 const Shipping = () => {
   const cart = useSelector((state) => state.cart)
@@ -21,60 +22,63 @@ const Shipping = () => {
   }
 
   return (
-    <div className="form-container">
-      <form onSubmit={submitHandler}>
-        <h1 className="profile-header">Shipping</h1>
-        <div className="form-group">
-          <label htmlFor="firstName">Address</label>
+    <>
+      <Breadcrumb step1 step2 />
+      <div className="shipping-container">
+        <form onSubmit={submitHandler}>
+          <h1 className="profile-header">Shipping</h1>
+          <div className="form-group">
+            <label htmlFor="firstName">Address</label>
+            <input
+              className="name-text-field"
+              placeholder="Enter address"
+              type="name"
+              id="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName">City</label>
+            <input
+              className="name-text-field"
+              placeholder="Enter city"
+              type="name"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Postal Code</label>
+            <input
+              className="name-text-field"
+              placeholder="Enter postal code"
+              type="name"
+              value={postalCode}
+              onChange={(e) => setPostalCode(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="pwd">Country</label>
+            <input
+              className="name-text-field"
+              placeholder="Enter country"
+              type="name"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+            />
+          </div>
           <input
-            className="name-text-field"
-            placeholder="Enter address"
-            type="name"
-            id="address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
+            style={{
+              marginBottom: '2vh',
+            }}
+            type="submit"
+            value="Continue"
+            className="btn btn-primary btn-block"
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="lastName">City</label>
-          <input
-            className="name-text-field"
-            placeholder="Enter city"
-            type="name"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Postal Code</label>
-          <input
-            className="name-text-field"
-            placeholder="Enter postal code"
-            type="name"
-            value={postalCode}
-            onChange={(e) => setPostalCode(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="pwd">Country</label>
-          <input
-            className="name-text-field"
-            placeholder="Enter country"
-            type="name"
-            value={country}
-            onChange={(e) => setCountry(e.target.value)}
-          />
-        </div>
-        <input
-          style={{
-            marginBottom: '2vh',
-          }}
-          type="submit"
-          value="Continue"
-          className="btn btn-primary btn-block"
-        />
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   )
 }
 
