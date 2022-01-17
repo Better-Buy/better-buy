41beb8d54b60035e34b2195fcbc73adef2c152d9
Kevin Shank <Kevin_Shank@yahoo.com>
2022-01-17 17:45:26 -0500

    Update mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,22 +1,24 @@
-//creating a mongoose connection. 
-const express = require("express");
-const mongoose = require('mongoose');
-const app = express();
+//creating a mongoose connection.
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const URI = process.env.MONGODB_URL;
+const URI = process.env.MONGODB_URI
 
-mongoose.connect(URI || 'mongodb://localhost/better-buy', {
-useNewUrlParser: true, 
+mongoose.connect(
+  URI || 'mongodb://localhost/Better-Buy',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (err) throw err
+    console.log('Connected to MongoDB!!!')
+  }
+)
 
-useUnifiedTopology: true 
-
-}, err => {
-if(err) throw err;
-console.log('Connected to MongoDB!!!')
-});
-
-
-module.exports = mongoose.connection;
-
+module.exports = mongoose.connection
