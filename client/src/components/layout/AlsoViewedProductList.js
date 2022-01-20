d1dbc0077df528558a68b80f7506cb9602cd114d
Kevin Shank <Kevin_Shank@yahoo.com>
2022-01-20 15:52:03 -0500

    Push

--- client/src/components/layout/AlsoViewedProductList.js
+++ client/src/components/layout/AlsoViewedProductList.js
@@ -4,11 +4,6 @@ import ProductRating from './ProductRating'
 import './ProductList.css'
 
 export default function AlsoViewedProductList(props) {
-  console.log(props.count)
-
-  const handleClick = (sku) => {
-    window.location.assign(`${sku}`)
-  }
   return (
     <Grid
       container
@@ -25,8 +20,6 @@ export default function AlsoViewedProductList(props) {
       sm={4}
       md={3}
       lg={3}
-      style={{ cursor: 'pointer' }}
-      onClick={() => handleClick(props.sku)}
     >
       <img
         className="product-main-image"
@@ -35,16 +28,6 @@ export default function AlsoViewedProductList(props) {
       />
       <div className="product-name">{props.name}</div>
       <div className="product-price">${props.price}</div>
-      <ProductRating customerRating={Number(props.score)} />
-      {props.count === 1 ? (
-        <div style={{ color: 'var(--dark-color)' }}>{props.count} review</div>
-      ) : null}
-      {props.count > 1 ? (
-        <div style={{ color: 'var(--dark-color)' }}>{props.count} reviews</div>
-      ) : null}
-      {props.count === null ? (
-        <div style={{ color: 'var(--dark-color)' }}>No reviews</div>
-      ) : null}
     </Grid>
   )
 }
